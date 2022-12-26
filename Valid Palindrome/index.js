aca97b325bc5d2bfed74dd29cb9b6d26e6f7d18c
Developstar <deyeye99@gmail.com>
2022-12-26 23:05:05 +0100

    Initial commit

---
+++ Valid Palindrome/index.js
@@ -0,0 +1,12 @@
+function isPalindrome(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    for (let i = 0; i < s.length / 2; i++) {
+      if (s[i] !== s[s.length - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+console.log(isPalindrome("sacecas"));  // true
+console.log(isPalindrome("hero"));   // false
