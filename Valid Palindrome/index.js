3027dad0291e5c07137152de27351f8635e5c1af
Developstar <deyeye99@gmail.com>
2022-12-27 23:05:56 +0100

    question added

--- Valid Palindrome/index.js
+++ Valid Palindrome/index.js
@@ -1,3 +1,5 @@
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
 function isPalindrome(s) {
     s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
     for (let i = 0; i < s.length / 2; i++) {
